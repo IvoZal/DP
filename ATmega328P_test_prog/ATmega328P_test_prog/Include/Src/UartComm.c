f50ddcb034459fe5195de23bfe8e2909c7865ed6
221032 <221032@vut.cz>
2023-12-05 22:03:40 +0100

    Copied few libraries from previous poject.

---
+++ ATmega328P_test_prog/ATmega328P_test_prog/Include/Src/UartComm.c
@@ -0,0 +1,135 @@
+/*
+ * UartComm.c
+ *
+ * Created: 11.04.2023 21:44:08
+ *  Author: Ivo Zaludek
+ */ 
+
+#include "UartComm.h"
+
+static bool bUserFlag = false;
+static char sUserName[MAX_USERNAME_LEN] = {'\0'};
+char cOpt;
+
+void UartHandler(uint8_t u8Status, uint8_t * u8Unlock, uint8_t * u8Lock)
+{
+	uint8_t u8Time[3];
+	RTC_read_time(u8Time);
+	switch (u8Status)
+	{
+		case 1:
+			printf("AutoOpen,%u:%u:%u\n",u8Time[0],u8Time[1],u8Time[2]);
+			break;
+			
+		case 2:
+			printf("UserOpen,%u:%u:%u\n",u8Time[0],u8Time[1],u8Time[2]);
+			break;
+			
+		case 3:
+			printf("Lock,%u:%u:%u\n",u8Time[0],u8Time[1],u8Time[2]);
+			break;
+			
+		case 4:
+			printf("AlarmOn,%u:%u:%u\n",u8Time[0],u8Time[1],u8Time[2]);
+			break;
+			
+		case 5:
+			printf("AlarmOff,%u:%u:%u\n",u8Time[0],u8Time[1],u8Time[2]);
+			break;
+			
+		case 6:
+			printf("PowerOn,%u:%u:%u\n",u8Time[0],u8Time[1],u8Time[2]);
+			break;
+	}
+	if(BufferLength() > 0)
+	{
+		switch (BufferGet())
+		{
+			case 'T':
+				cOpt = BufferGet();
+				uint8_t u8SetTime[3];
+				for(uint8_t u8i = 0; u8i < 3; u8i++) {
+					BufferGet();
+					u8SetTime[u8i] = (BufferGet() - 0x30) * 10;
+					u8SetTime[u8i] += BufferGet() - 0x30;
+				}
+				switch (cOpt)
+				{
+					case 'S':
+						RTC_set_time(u8SetTime);
+						printf("Time was set.\n");
+						break;
+						
+					case 'U':
+						memcpy(u8Unlock, u8SetTime, 3);
+						SaveTimes(u8Unlock,u8Lock);
+						printf("Auto unlock time set.\n");
+						break;
+						
+					case 'L':
+						memcpy(u8Lock, u8SetTime, 3);
+						SaveTimes(u8Unlock,u8Lock);
+						printf("Auto lock time set.\n");
+						break;
+				}
+				break;
+				
+			case 'U':
+				bUserFlag = true;
+				BufferGet();
+				for(uint8_t u8j = 0; (BufferLength() > 0) & (u8j < MAX_USERNAME_LEN); u8j++) {
+					sUserName[u8j] = BufferGet();
+				}
+				break;
+				
+			case 'P':
+				if(bUserFlag == true) {
+					char sPassw[MAX_PASSW_LEN] = {'\0'};
+					BufferGet();
+					uint8_t u8j = 0;
+					for(; (BufferLength() > 0) & (u8j < MAX_PASSW_LEN); u8j++) {
+						sPassw[u8j] = BufferGet();
+					}
+					if(u8j > 3) {		// password is longer than 4 chars
+						bUserFlag = false;
+						if(SaveUser(sUserName,sPassw))
+							printf("User %s added.\n",sUserName);
+						else
+							printf("Memory is full.\n");
+						for (uint8_t u8i = 0; u8i < MAX_USERNAME_LEN; u8i++)
+							sUserName[u8i] = '\0';
+					}
+					else
+						printf("Password must be longer than 4 chars.\n");					
+					break;
+				}
+				else {
+					while(BufferLength() > 0)
+						BufferGet();
+				}
+				
+			case 'D':
+				BufferGet();
+				char sDelUserName[MAX_USERNAME_LEN] = {'\0'};
+				for(uint8_t u8j = 0; (BufferLength() > 0) & (u8j < MAX_USERNAME_LEN); u8j++) {
+					sDelUserName[u8j] = BufferGet();
+				}
+				bool bDelFlag = DeleteUser(sDelUserName);
+				if(bDelFlag)
+					printf("User %s deleted.\n",sDelUserName);
+				else
+					printf("Username not found.\n");
+				break;
+				
+			case 'E':
+				if(BufferGet() == 'M') {
+					DeleteAll();
+					printf("Memory erased.\n");
+				}
+				
+			default:
+				while(BufferLength() > 0)
+					BufferGet();
+		}
+	}
+}
---
+++ Main_test_prog/Main_test_prog/Include/Header/keyboard.h
@@ -0,0 +1,40 @@
+/*
+ * keyboard.h - library for matrix keyboard
+ *
+ * Created: 16.03.2023 20:31:06
+ *  Author: Ivo Zaludek
+ *
+ * This header file contains function and variable declarations for
+ * the lcd.h library. These declarations are needed by any code
+ * that uses the library, so this file should be included in all source
+ * files that use the library.
+ *
+ * Note: This file should be included after any system or third-party
+ * header files that are needed by the library. 
+ */ 
+
+
+#ifndef KEYBOARD_H_
+#define KEYBOARD_H_
+
+#include <avr/io.h>
+#include "timer.h"
+
+/* Pinout */
+#define Row1 (1 << DDB3)
+#define Row2 (1 << DDB2)
+#define Row3 (1 << DDB1)
+#define Row4 (1 << DDB0)
+#define Col1 (1 << DDC3)
+#define Col2 (1 << DDC2)
+#define Col3 (1 << DDC1)
+#define Col4 (1 << DDC0)
+
+/* Function returns pressed button on matrix keyboard as a char. */
+char KeyboardRead();
+
+/* KeyboardRead function with delay used to avoid registering multiple key presses if the same key is pressed too quickly. */
+char KeyboardRead_OncePressed();
+
+
+#endif /* KEYBOARD_H_ */
