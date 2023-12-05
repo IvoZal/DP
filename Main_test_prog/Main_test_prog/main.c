d882f683041d202a1cd172aba978778c92a04978
221032 <221032@vut.cz>
2023-12-05 21:42:57 +0100

    Created projects for both MCUs. Added gitignores.

---
+++ Main_test_prog/Main_test_prog/main.c
@@ -0,0 +1,18 @@
+/*
+ * Main_test_prog.c
+ *
+ * Created: 05.12.2023 21:35:24
+ * Author : ACER_E15
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
