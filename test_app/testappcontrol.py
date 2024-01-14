25967a31578373b7f3e0458664b9800128305454
221032 <221032@vut.cz>
2024-01-14 20:30:35 +0100

    Added start and exit button.

--- test_app/testappcontrol.py
+++ test_app/testappcontrol.py
@@ -7,4 +7,11 @@ class TestAppController:
         self.test_devices = {"ATmega328P Xplained Mini" : True,
                     "Modul LCD displeje" : True,
                     "Rele modul" : True,
-                    "Rotacni enkoder" : True}
+                    "Rotacni enkoder" : True,
+                    "RTC a EEPROM modul" : True,
+                    "Modul s termistorem" : True,
+                    "Reproduktor" : True,
+                    "Maticova klavesnice" : True}
+        
+    def start_clicked(self):
+        print("Start")
--- test_app/testappview.py
+++ test_app/testappview.py
@@ -2,17 +2,25 @@ import tkinter as tk
 
 class DefaultView:
     def __init__(self, root, controller):
+        self.root = root
         self.controller = controller
         self.check_vars = {}
         
-        start_btn = tk.Button(root, text="Start", font=("Arial",14), background='green')
+        test_label = tk.Label(self.root, text="Vyberte, ktere moduly testovat:       ", font=("Arial",14))
+        test_label.grid(column=0,row=0, sticky=tk.E, padx=5, pady=5)
+
+        start_btn = tk.Button(self.root, text="Start", font=("Arial",14), background='green', command=self.controller.start_clicked)
         start_btn.grid(column=1,row=0, sticky=tk.E, padx=5, pady=5)
-        i = 0
+
+        exit_btn = tk.Button(self.root, text="Exit", font=("Arial",14), background='red', command=lambda: self.root.quit())
+        exit_btn.grid(column=1, row=1, sticky=tk.E, padx=5, pady=5)
+
+        i = 1
         for device, initial_state in self.controller.test_devices.items():
             check_var = tk.BooleanVar(value=initial_state)
             self.check_vars[device] = check_var
 
-            check_btn = tk.Checkbutton(root, text=device, variable=check_var, font=("Arial", 14),
+            check_btn = tk.Checkbutton(self.root, text=device, variable=check_var, font=("Arial", 14),
                                         command=lambda d=device: self.update_state(d))
             check_btn.grid(row=i, column=0, padx=5, pady=5, sticky=tk.W)
             i += 1
