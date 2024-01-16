4e35a47ea8625c4546445caf9ff2ade83303898c
221032 <221032@vut.cz>
2024-01-16 21:31:36 +0100

    Moduls sorted by the type of test.

--- test_app/testappcontrol.py
+++ test_app/testappcontrol.py
@@ -5,13 +5,13 @@ class TestAppController:
         self.model = model
         self.view = view
         self.test_devices = {"ATmega328P Xplained Mini" : True,
-                    "Modul LCD displeje" : True,
-                    "Rele modul" : True,
-                    "Rotacni enkoder" : True,
-                    "RTC a EEPROM modul" : True,
-                    "Modul s termistorem" : True,
-                    "Reproduktor" : True,
-                    "Maticova klavesnice" : True}
+                             "Rele modul" : True,
+                             "RTC a EEPROM modul" : True,
+                             "Rotacni enkoder" : True,
+                             "Maticova klavesnice" : True,
+                             "Modul LCD displeje" : True,
+                             "Reproduktor" : True,
+                             "Modul s termistorem" : True,}
         
     def start_clicked(self):
         print("Start")
