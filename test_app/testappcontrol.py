17a6e92cbfdd09953663d09637bbc43c2327bf84
221032 <221032@vut.cz>
2024-01-19 08:37:54 +0100

    Updated test_device data structure.

--- test_app/testappcontrol.py
+++ test_app/testappcontrol.py
@@ -1,17 +1,77 @@
 import tkinter as tk
 
+class Device:
+    def __init__(self, name):
+        self.name = name
+        self.state = True
+        self.tk_var = tk.BooleanVar(value=self.state)
+        self.result = "FAIL"
+        self.err_message = ""
+
 class TestAppController:
     def __init__(self, model, view):
         self.model = model
         self.view = view
-        self.test_devices = {"ATmega328P Xplained Mini" : True,
-                             "Rele modul" : True,
-                             "RTC a EEPROM modul" : True,
-                             "Rotacni enkoder" : True,
-                             "Maticova klavesnice" : True,
-                             "Modul LCD displeje" : True,
-                             "Reproduktor" : True,
-                             "Modul s termistorem" : True,}
+        self.test_devices = [Device("ATmega328P Xplained Mini"),
+                             Device("Rele modul"),
+                             Device("RTC a EEPROM modul"),
+                             Device("Rotacni enkoder"),
+                             Device("Maticova klavesnice"),
+                             Device("Modul LCD displeje"),
+                             Device("Reproduktor"),
+                             Device("Modul s termistorem"),]
         
     def start_clicked(self):
         print("Start")
+        for device in self.test_devices:
+            match device.name:
+                case "ATmega328P Xplained Mini":
+                    if(device.state):
+                        test_result = self.model.self_test("TEST ATMEGA")
+                        if(test_result[0]):
+                            device.result = "PASS"
+                        else:
+                            device.result = "FAIL"
+                            device.err_message = test_result[1]
+                    else:
+                        device.result = "SKIPPED"
+
+                case "Rele modul":
+                    if(device.state):
+                        test_result = self.model.self_test("TEST RELAY")
+                        if(test_result[0]):
+                            device.result = "PASS"
+                        else:
+                            device.result = "FAIL"
+                            device.err_message = test_result[1]
+                    else:
+                        device.result = "SKIPPED"
+
+                case "RTC a EEPROM modul":
+                    if(device.state):
+                        test_result = self.model.self_test("TEST RTC")
+                        if(test_result[0]):
+                            device.result = "PASS"
+                        else:
+                            device.result = "FAIL"
+                            device.err_message = test_result[1]
+                    else:
+                        device.result = "SKIPPED"
+
+                case "Rotacni enkoder":
+                    pass
+
+                case "Maticova klavesnice":
+                    pass
+
+                case "Modul LCD displeje":
+                    pass
+
+                case "Reproduktor":
+                    pass
+
+                case "Modul s termistorem":
+                    pass
+
+                case others:
+                    pass
--- test_app/testappview.py
+++ test_app/testappview.py
@@ -4,7 +4,6 @@ class DefaultView:
     def __init__(self, root, controller):
         self.root = root
         self.controller = controller
-        self.check_vars = {}
         
         test_label = tk.Label(self.root, text="Vyberte, ktere moduly testovat:       ", font=("Arial",14))
         test_label.grid(column=0,row=0, sticky=tk.E, padx=5, pady=5)
@@ -16,15 +15,13 @@ class DefaultView:
         exit_btn.grid(column=1, row=1, sticky=tk.E, padx=5, pady=5)
 
         i = 1
-        for device, initial_state in self.controller.test_devices.items():
-            check_var = tk.BooleanVar(value=initial_state)
-            self.check_vars[device] = check_var
-
-            check_btn = tk.Checkbutton(self.root, text=device, variable=check_var, font=("Arial", 14),
+        for device in self.controller.test_devices:
+            check_btn = tk.Checkbutton(self.root, text=device.name, variable=device.tk_var, font=("Arial", 14),
                                         command=lambda d=device: self.update_state(d))
             check_btn.grid(row=i, column=0, padx=5, pady=5, sticky=tk.W)
             i += 1
 
     def update_state(self, device):
-        self.controller.test_devices[device] = self.check_vars[device].get()
-        print(f"State of {device}: {self.controller.test_devices[device]}")
+        print(device.tk_var)
+        device.state = device.tk_var.get()
+        print(f"State of {device.name}: {device.state}")
