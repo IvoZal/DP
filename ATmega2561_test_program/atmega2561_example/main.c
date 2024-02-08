159ab8bba9f21b460020c2517d2147e411f0058b
221032 <221032@vut.cz>
2024-02-08 13:56:54 +0100

    Added serial commands, added calling in loop for repeating tests.

--- ATmega2561_test_program/atmega2561_example/main.c
+++ ATmega2561_test_program/atmega2561_example/main.c
@@ -20,10 +20,15 @@ int main(void)
 	atmel_start_init();
 	
 	UART_CMD_T cmd_lut[] =
-		{{"dio_high",printf},
-		{"dio_low",printf},
-		{"dio_read",(callback)printf},
-		{"aio_read",(callback)printf}};
+		{{"STOP",printf},
+		{"TEST ATMEGA",printf},
+		{"TEST RELAY",printf},
+		{"TEST RTC",printf},
+		{"TEST ENCODER",printf},
+		{"TEST KEYBRD",printf},
+		{"TEST LCD",printf},
+		{"TEST REPRO",printf},
+		{"TEST THERM",printf}};
 			
 	char input_string[BUFFER_SIZE];
 	
@@ -31,29 +36,33 @@ int main(void)
 	ENABLE_INTERRUPTS();
 
 	uint8_t max_i = sizeof(cmd_lut) / sizeof(UART_CMD_T);
+	uint8_t i = 0;
 
 	while (1) 
 	{
 		if(BufferPeekLast() == 0xA)	// if the last char was line feed
 		{
-			uint8_t i = 0;
-			while(BufferLength() > 0)
+			for(uint8_t j = 0; BufferLength() > 0; j++)
 			{
-				input_string[i] = BufferGet();
-				++i;
+				input_string[j] = BufferGet();
 			}
 			for(i=0; i < max_i; i++)
 			{
 				if(strstr(input_string,cmd_lut[i].cmd) != NULL)
 				{
-					cmd_lut[i].cb(input_string);
 					break;
 				}
 			}
-			if(i == max_i)
-			{
-				printf("ERROR: Unsupported command!\n");
-			}
+		}
+		if(i == max_i)
+		{
+			printf("ERROR: Unsupported command!\n");
+		}
+		else if(i > 0)	// if a command was found in the input string, process it
+		{
+			cmd_lut[i].cb(input_string);
+			if (i <= 3)	// for non repeating tests
+				i = 0;
 		}
 	}
 }
